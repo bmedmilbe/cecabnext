7018506e147d3b38811cb66f452e3d42a0e7d169
bmedmilbe <bm.edmilbe@gmail.com>
2026-02-28 19:55:14 +0000

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+// import "../styles/bootstrap.min.css";
+import "./styles/fontawesome.min.css";
+import "./styles/animate.min.css";
+import "./styles/flaticon.css";
+import "swiper/css";
+import "swiper/css/bundle";
+// Global Style
+import "./styles/style.css";
+import "./styles/responsive.css";
+import "./styles/index.css";
+import "./styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
