8e06ce5119ed4af929b166342272a5974ae5126b
bmedmilbe <bm.edmilbe@gmail.com>
2026-03-04 05:01:58 +0000

    Added metadata for a blog page

--- app/(marketing)/blogs/(home)/page.tsx
+++ app/(marketing)/blogs/(home)/page.tsx
@@ -1,6 +1,30 @@
-import { PostListAction } from "@/app/actions/posts";
+import { PostAction, PostListAction } from "@/app/actions/posts";
 import BlogCardsInfinity from "@/app/components/infinities/BlogCardsInfinity";
 
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const slug = (await params).slug;
+
+  const post = await PostAction(slug);
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: post.data?.title,
+    description: post.data?.beginnig,
+    openGraph: {
+      images: [post.data?.image || "", ...previousImages],
+    },
+  };
+}
 const BlogsPage = async () => {
   const postsi = await PostListAction();
   return (
