57c0a4044b1410fbe946668e212bfb4a2b93a4d9
bmedmilbe <bm.edmilbe@gmail.com>
2026-03-05 05:10:56 +0000

    Remove caching for api request

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -15,6 +15,7 @@ const apiRequest = async <T>(
         "Content-Type": "application/json",
         ...params.headers,
       },
+      cache: "no-store",
     });
     const playload = await response.json().catch(() => null);
 
